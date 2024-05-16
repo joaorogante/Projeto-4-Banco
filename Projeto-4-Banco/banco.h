b1214bbe7d2ef1bbcbe007b0c5c13d6c86e9c6fe
joaorogante <143849096+joaorogante@users.noreply.github.com>
2024-05-16 10:03:29 -0300

    Update banco.h

--- Projeto-4-Banco/banco.h
+++ Projeto-4-Banco/banco.h
@@ -1,6 +1,22 @@
-#include <stdio.h>
+#ifndef CLIENTE_H
+#define CLIENTE_H
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
-}
+typedef struct {
+    char nome[50];
+    char cpf[11];
+    int tipo_conta;
+    double saldo;
+    char senha[20];
+} Cliente;
+
+void salvar_dados(Cliente *clientes, int num_clientes);
+void carregar_dados(Cliente *clientes, int *num_clientes);
+void novo_cliente(Cliente *clientes, int *num_clientes);
+void apaga_cliente(Cliente *clientes, int *num_clientes);
+void listar_clientes(Cliente *clientes, int num_clientes);
+void debito(Cliente *clientes, int num_clientes);
+void deposito(Cliente *clientes, int num_clientes);
+void extrato(Cliente *clientes, int num_clientes);
+void transferencia(Cliente *clientes, int num_clientes);
+
+#endif
